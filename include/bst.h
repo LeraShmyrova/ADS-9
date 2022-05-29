53de1dcf0acbd793438642255d63dad261b676c8
LeraShmyrova <94710005+LeraShmyrova@users.noreply.github.com>
2022-05-29 12:43:28 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -2,4 +2,68 @@
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
 
+template<typename T>
+class BST {
+ private:
+  struct Node {
+  int ccut; 
+  Node *prrav;
+  Node *leevv; 
+  T vallu;
+  };
+  Node *rt;
+
+  Node* addNode(Node* rt, T vallu) {
+    if (rt == nullptr) {
+      rt = new Node;
+      rt->vallu = vallu;
+      rt->ccut = 1;
+      rt->leevv = rt->prrav = nullptr;
+    }else if (rt->vallu > vallu) {
+      rt->prrav = addNode(rt->prrav, vallu);
+    }else if (rt->vallu < vallu) {
+      rt->leevv = addNode(rt->leevv, vallu);
+    }else{
+      rt->ccut++;
+    }
+      return rt;
+  }
+
+  int searchNode(Node* rt, T vallu) {
+    if (rt == nullptr ){
+      return 0;
+    }else if (rt->vallu < vallu) {
+      return searchNode(rt->leevv, vallu); 
+    }else if (rt->vallu > vallu) {
+      return searchNode(rt->prrav, vallu);
+    }else{
+      return rt->ccut;
+    }  
+  }
+
+  int hTee(Node* rt) {
+    if (rt == nullptr) {
+      return 0;
+    }  
+    if (rt->leevv == nullptr && rt->prrav == nullptr) {
+        return 0;
+    }  
+    int lvl = hTee(rt->leevv);
+    int rvr = hTee(rt->prrav);
+    return rvr > lvl ? rvr + 1 : lvl + 1;
+  }
+
+ public:
+  BST() : rt(nullptr) {}
+  void add(T vallu) {
+    rt = addNode(rt, vallu);
+  }
+  int sch(T vallu) {
+    return searchNode(rt, vallu);
+  }
+  int dth() {
+    return hTee(rt);
+  }
+};
+
 #endif  // INCLUDE_BST_H_
