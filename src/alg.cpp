fb2b428752cf706f9f7a966d6758547988d5f54d
LeraShmyrova <94710005+LeraShmyrova@users.noreply.github.com>
2022-05-29 13:09:16 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,7 +4,26 @@
 #include  <locale>
 #include  <cstdlib>
 #include  "bst.h"
-
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+BST<std::string> bsst;
+  std::ifstream file(filename);
+  std::string slovvo = "";
+  char le = 'a' - 'A';
+  if (!file.is_open()) {
+  throw std::string("Smth is wrong!");
+  }
+  while (!file.eof()) {
+  char tmprrr = file.get();
+  if ((tmprrr >= 'a' && tmprrr <= 'z') || (tmprrr >= 'A' && tmprrr <= 'Z')) {
+  if (tmprrr >= 'A' && tmprrr <= 'Z') {
+  slovvo += tmprrr;
+  tmprrr += le;
+  }
+  } else if (slovvo != "") {
+  bsst.add(slovvo);
+  slovvo = "";
+  }
+  }
+  file.close();
+  return bsst;
 }
